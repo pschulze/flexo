2c76ef8b4a8c8da95f69ab020550fc8dcbc41a0f
Peter Schulze <peterkschulze@gmail.com>
2019-12-02 12:40:27 -0700

    add custom errors

--- src/flexo/result.cr
+++ src/flexo/result.cr
@@ -1,14 +1,27 @@
 require "stumpy_png"
 require "stumpy_core"
 
+
+
 module Flexo
   class Result
+    class ThresholdValueException < Exception
+    end
+
     getter score
     setter score
     getter difference_image
     setter difference_image
 
     def initialize(@threshold : Float32)
+      if @threshold < 0.0
+        raise ThresholdValueException.new(
+          "threshold must be greater than or equal to 0.0")
+      elsif @threshold > 1.0
+        raise ThresholdValueException.new(
+          "threshold can be no greater than 1.0")
+      end
+
       @score = 1.0
       @difference_image = StumpyCore::Canvas.new(0, 0)
     end
